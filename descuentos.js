9f4dde8132c75e242813f3ed074d760c9ce873dd
Harrison Martinez <jonathan.martinez@corusconsulting.com>
2022-03-12 12:24:11 -0500

    Clase 12 - Escribiendo HTML desde JavaScript

--- descuentos.js
+++ descuentos.js
@@ -14,3 +14,16 @@ function calcularPrecioConDescuento(precio, descuento){
     porcentajePrecioConDescuento,
     precioConDescuento
 });*/
+
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
