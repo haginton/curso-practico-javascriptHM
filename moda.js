007884fa6d8bc576f43b6a6851825f77670356a9
Harrison Martinez <jonathan.martinez@corusconsulting.com>
2022-04-23 20:47:59 -0500

    Clase 17: Calculando la moda en Javascript despues

--- moda.js
+++ moda.js
@@ -1,33 +1,23 @@
-const lista1 = [
-    1,
-    2,
-    3,
-    1,
-    2,
-    3,
-    4,
-    2,
-    2,
-    2,
-    1
-];
+function calcularModa (miLista){ // ej argumento funcion: [1,1,1,1,2,2,2,2,2,6,6,6,6,6,7,7,7,4,4,4,4,4,4,4,4,4,4,4,9,51,3,6,8,4,6,8,9,41,1,2,6]
+    const listaCount = {};
 
-const lista1Count = {};
-
-lista1.map(
+    miLista.map(
     function (elemento){
-        if (lista1Count[elemento]){
-            lista1Count[elemento] += 1;
-        } else {
-            lista1Count[elemento] = 1;
+            if (listaCount[elemento]){
+                listaCount[elemento] += 1;
+            } else {
+                listaCount[elemento] = 1;
+            }
         }
-    }
-);
+    );
 
-const lista1Array = Object.entries(lista1Count).sort(
-    function(elementoA, elementoB){
-        return elementoA[1] - elementoB[1];
-    }
-);
+    const listaArray = Object.entries(listaCount).sort( // Object.entries(objeto) esto convierte el objeto pasado como argumento en un array
+        function(elementoA, elementoB){
+            return elementoA[1] - elementoB[1];
+        }
+    );
 
-const moda = lista1Array[lista1Array.length - 1];
+    const moda = listaArray[listaArray.length - 1];
+
+    return moda;
+}
